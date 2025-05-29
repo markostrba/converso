3707ebda5a9581f5d84520759cc84ade6865b330
Marko Strba <mark3strba@gmail.com>
2025-05-29 23:32:51 +0200

    Implement Companion library page

--- types/index.d.ts
+++ types/index.d.ts
@@ -27,3 +27,14 @@ interface CreateCompanion {
   style: string;
   duration: number;
 }
+
+interface GetAllCompanions {
+  limit?: number;
+  page?: number;
+  subject?: string | string[];
+  topic?: string | string[];
+}
+
+interface SearchParams {
+  searchParams: Promise<{ [key: string]: string | string[] | undefined }>;
+}
