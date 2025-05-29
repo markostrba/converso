c518f795575e5e290a01bad6081855a53a9523a5
Marko Strba <mark3strba@gmail.com>
2025-05-29 04:08:25 +0200

    Add Navbar

---
+++ components/Navbar.tsx
@@ -0,0 +1,21 @@
+import Link from "next/link";
+import ROUTES from "@/constants/routes";
+import Image from "next/image";
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <Link href={ROUTES.HOME}>
+        <div className="flex items-center gap-2.5 cursor-pointer">
+          <Image src="/images/logo.svg" alt="logo" width={46} height={44} />
+        </div>
+      </Link>
+      <div className="flex items-center gap-8">
+        <Link href={ROUTES.SIGN_IN}>
+          <p>Sign In</p>
+        </Link>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
---
+++ constants/routes.ts
@@ -0,0 +1,7 @@
+const ROUTES = {
+  HOME: "/",
+  COMPANIONS: "/companions",
+  MY_JOURNEY: "/my-journey",
+  SIGN_IN: "/sign-in"
+}
+export default ROUTES
